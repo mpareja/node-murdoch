db4e9955e8a8477c10c4a4d10040d1a36cda2e11
Mario Pareja <pareja.mario@gmail.com>
2013-05-28 09:08:38 -0400

    Generate rupert test data for reporting.

--- index.js
+++ index.js
@@ -3,6 +3,48 @@ var express = require('express'),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server);
 
+var tasks = {};
+for (var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+  var c = String.fromCharCode(i);
+  tasks[c] = function (cb) {
+    setTimeout(function () { cb(null); }, Math.random() * 2500.0);
+  };
+}
+
+var plan = {
+  'a': ['b', 'c', 'd'],
+  'b': ['c', 'd', 'e'],
+  'c': ['d', 'e', 'f'],
+  'd': ['e', 'f', 'g'],
+  'e': ['f', 'g', 'h'],
+
+  'f': ['p', 'q', 'r'],
+  'g': ['q', 'r', 's'],
+  'h': ['r', 's', 't'],
+
+  'i': ['u', 'v', 'w'],
+  'j': ['v', 'w', 'x'],
+  'k': ['w', 'x', 'y'],
+  'l': ['x', 'y', 'z'],
+  'm': ['y', 'z', 'u'],
+  'n': ['z', 'u', 'v'],
+  'o': ['u', 'v', 'w'],
+
+  'p': ['i', 'j', 'k'],
+  'q': ['j', 'k', 'l'],
+  'r': ['k', 'l', 'm'],
+  's': ['l', 'm', 'n'],
+  't': ['m', 'n', 'o'],
+
+  'u': ['v', 'w', 'x'],
+  'v': ['w', 'x', 'y'],
+  'w': ['x', 'y', 'z'],
+  'x': ['y', 'z'],
+  'y': ['z'],
+  'z': [],
+};
+
+var rupert = require('rupert');
 app.use(express.static('public'));
 server.listen(8080);
 
@@ -11,10 +53,13 @@ sockets = [];
 io.sockets.on('connection', function (socket) {
   sockets.push(socket);
 
-  socket.emit('init', {
-    "applyChangesToReporting": [],
-    "updateLanguageStrings": [],
-    "deployDbChanges": ['updateLanguageStrings', 'applyChangesToReporting']
+  socket.emit('init', plan);
+
+  var r = rupert(tasks, plan, function (err) {
+    socket.disconnect();
+  });
+  r.on('taskComplete', function (task) {
+    socket.emit('complete', task);
   });
 });
 
