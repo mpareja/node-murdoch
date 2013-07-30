578f3d24946a52836f4a83a7c90a0d7ca3bf0d68
Mario Pareja <pareja.mario@gmail.com>
2013-07-30 09:44:10 -0400

    server: using shoe and emit-stream.

--- index.js
+++ index.js
@@ -1,78 +1,75 @@
-var express = require('express'),
-  app = express(),
-  server = require('http').createServer(app),
-  io = require('socket.io').listen(server);
-
-var tasks = {};
-for (var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
-  var c = String.fromCharCode(i);
-  tasks[c] = function (cb) {
-    setTimeout(function () { cb(null); }, Math.random() * 2500.0);
-  };
-}
-
-var plan = {
-  'a': ['b', 'c', 'd'],
-  'b': ['c', 'd', 'e'],
-  'c': ['d', 'e', 'f'],
-  'd': ['e', 'f', 'g'],
-  'e': ['f', 'g', 'h'],
-
-  'f': ['p', 'q', 'r'],
-  'g': ['q', 'r', 's'],
-  'h': ['r', 's', 't'],
-
-  'i': ['u', 'v', 'w'],
-  'j': ['v', 'w', 'x'],
-  'k': ['w', 'x', 'y'],
-  'l': ['x', 'y', 'z'],
-  'm': ['y', 'z', 'u'],
-  'n': ['z', 'u', 'v'],
-  'o': ['u', 'v', 'w'],
-
-  'p': ['i', 'j', 'k'],
-  'q': ['j', 'k', 'l'],
-  'r': ['k', 'l', 'm'],
-  's': ['l', 'm', 'n'],
-  't': ['m', 'n', 'o'],
-
-  'u': ['v', 'w', 'x'],
-  'v': ['w', 'x', 'y'],
-  'w': ['x', 'y', 'z'],
-  'x': ['y', 'z'],
-  'y': ['z'],
-  'z': [],
-};
-
-var rupert = require('rupert');
-app.use(express.static('public'));
-server.listen(8080);
-
-sockets = [];
-
-io.sockets.on('connection', function (socket) {
-  sockets.push(socket);
-
-  socket.emit('init', plan);
-
-  var r = rupert(tasks, plan, function (err) {
-    socket.disconnect();
-  });
-  r.on('taskComplete', function (task) {
-    socket.emit('complete', task);
-  });
+var shoe = require('shoe');
+var es = require('emit-stream');
+var socket = shoe(function (stream) {
+  var tp = getTaskPlan();
+  var r = rupert(tp.tasks, tp.plan, function (err) {});
+  es(r).pipe(stream);
+  r.emit('init', tp.plan);
 });
 
-var net = require("net"),
-    repl = require("repl");
+enableAutoBrowserify();
 
-net.createServer(function (socket) {
-  repl.start({
-    prompt: "murdoch: ",
-    input: socket,
-    output: socket
-  }).on('exit', function() {
-    socket.end();
-  });
-}).listen(8082);
- 
+var http = require('http');
+var ecstatic = require('ecstatic')(__dirname + '/public');
+var server = http.createServer(ecstatic);
+socket.install(server, '/stream');
+server.listen(8080);
+
+function getTaskPlan() {
+  var tasks = {};
+  for (var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+    var c = String.fromCharCode(i);
+    tasks[c] = function (cb) {
+      setTimeout(function () { cb(null); }, Math.random() * 2500.0);
+    };
+  }
+
+  var plan = {
+    'a': ['b', 'c', 'd'],
+    'b': ['c', 'd', 'e'],
+    'c': ['d', 'e', 'f'],
+    'd': ['e', 'f', 'g'],
+    'e': ['f', 'g', 'h'],
+
+    'f': ['p', 'q', 'r'],
+    'g': ['q', 'r', 's'],
+    'h': ['r', 's', 't'],
+
+    'i': ['u', 'v', 'w'],
+    'j': ['v', 'w', 'x'],
+    'k': ['w', 'x', 'y'],
+    'l': ['x', 'y', 'z'],
+    'm': ['y', 'z', 'u'],
+    'n': ['z', 'u', 'v'],
+    'o': ['u', 'v', 'w'],
+
+    'p': ['i', 'j', 'k'],
+    'q': ['j', 'k', 'l'],
+    'r': ['k', 'l', 'm'],
+    's': ['l', 'm', 'n'],
+    't': ['m', 'n', 'o'],
+
+    'u': ['v', 'w', 'x'],
+    'v': ['w', 'x', 'y'],
+    'w': ['x', 'y', 'z'],
+    'x': ['y', 'z'],
+    'y': ['z'],
+    'z': [],
+  };
+
+  return { tasks: tasks, plan: plan };
+};
+
+function enableAutoBrowserify() {
+  var watchify = require('watchify')(['./public/client.js']);
+  watchify.on('update', updateBundle);
+  updateBundle();
+
+  function updateBundle() {
+    console.log('Updating bundle.');
+    var bundle = watchify.bundle();
+    var file = require('fs').createWriteStream('./public/client.min.js');
+    bundle.pipe(file);
+  }
+}
+
